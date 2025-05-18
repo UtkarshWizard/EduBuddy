5ba883f02b241b74aca4f53b70b021d9eec29de7
UtkarshWizard <utkarshr1214@gmail.com>
2025-05-18 15:36:48 +0530

    created route to create space and to join space

--- apps/HTTP-BE/src/index.ts
+++ apps/HTTP-BE/src/index.ts
@@ -2,8 +2,9 @@ import express from "express";
 import { prismaClient } from "@repo/db/client";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import jwt from "jsonwebtoken";
-import { SignInSchema, SignUpSchema } from "@repo/common/types";
+import { SignInSchema, SignUpSchema, SpaceSchema } from "@repo/common/types";
 import bcrypt from "bcrypt";
+import { middleware } from "./middleware";
 
 const app = express();
 app.use(express.json());
@@ -101,6 +102,67 @@ app.post('/signin' , async (req , res) => {
         })
     }
 
+})
+
+app.post('/createSpace', middleware , async (req , res) => {
+    const parsedData = SpaceSchema.safeParse(req.body);
+
+    if (!parsedData.success) {
+        res.status(403).json({
+            message: "Invalid Inputs"
+        })
+        return
+    }
+
+    const userId = req.userId;
+
+    try {
+        const space = await prismaClient.space.create({
+            data: {
+                name: parsedData.data.name,
+                subject: parsedData.data.subject,
+                adminId: userId
+            }
+        })
+        res.json({
+            spaceId: space.id,
+            message: 'Space created Succesfully'
+        })
+    } catch (e) {
+        res.status(403).json({
+            message: "Failed to create Space"
+        })
+    }
+})
+
+app.post('/space/:id', middleware , async (req , res) => {
+    const spaceId = Number(req.params.id);
+    const userId = req.userId;
+
+    if (!spaceId || !userId) {
+        res.status(403).json({
+            message: "Invalid space/user Id"
+        })
+        return
+    }
+
+    try {
+        const participant = await prismaClient.spaceParticipant.create({
+            data: {
+                spaceId,
+                userId,
+                role: "member"
+            }
+        })
+        res.json({
+            messsage: "Joined Space Succesfully",
+            participantId: participant.id
+        })
+    } catch (e) {
+        res.status(403).json({
+            message : "Failed to join space"
+        })
+    }
 })
 
 app.listen(3001);
